8d7bb56f0e79a730a254ae8298cb91e56ef486c6
DenisLorand <denis.lorand@yahoo.com>
2021-01-28 14:57:32 +0100

    ajout3

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
-echo ('Hello World !');
+
+require('vendor/autoload.php');
+
+// $_SERVER['REQUEST_URI']
+
+// Si mon url != "
+
+if($_SERVER['HTTP_HOST'] != "api-slack-token-dl.herokuapp.com"){
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+}
+
+// sinon je ne le fais pas
+$ary = array("token" => $_ENV['TOKEN']);
+$data = json_encode($ary);
+
+
+// [{data: "Hello world !"}]
+
+echo ($data);
+
 ?>
